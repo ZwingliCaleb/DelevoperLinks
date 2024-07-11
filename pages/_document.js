3b0bc0bda09757a435c7f15edae81b9a5cf90bb3
ZwingliCaleb <calebzwingli@gmail.com>
2024-07-11 20:45:07 +0300

    Starting out with the index page

---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        {/* You can add custom fonts, meta tags, or other elements here */}
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
